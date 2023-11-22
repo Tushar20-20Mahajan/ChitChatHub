fe1ba83e085a260ea7666d761ed2fb4206b252e5
Tushar Mahajan <tusharmahajan080@gmail.com>
2023-11-23 02:13:29 +0530

    Changes Done and Files Added

--- chitchathub-frontend/src/components/Navigation.js
+++ chitchathub-frontend/src/components/Navigation.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {LinkContainer} from 'react-router-bootstrap'
 function Navigation() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
@@ -11,7 +12,12 @@ function Navigation() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <LinkContainer to="/login">
+            <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/chat">
+            <Nav.Link>Chat</Nav.Link>
+            </LinkContainer>
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
