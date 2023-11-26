f6c755fbbfac76807698459d5a44039d56a94756
Tushar Mahajan <tusharmahajan080@gmail.com>
2023-11-27 01:29:32 +0530

    Changes Done and Files Added

--- chitchathub-frontend/src/pages/Login.js
+++ chitchathub-frontend/src/pages/Login.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Button, Form, Container, Row, Col } from "react-bootstrap";
+import {Link} from "react-router-dom";
+import "./Login.css";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <Container>
+      <Row>
+        <Col md={5} className="login__bg"></Col>
+        <Col
+          md={7}
+          className="d-flex flex-direction-column align-items-center justify-content-center"
+        >
+          <Form style={{ width: "80%", maxWidth: 500 }}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+            <div className="py-4">
+              <p className="text-center"> Don't have an account? <Link to="/signup">Sign-Up</Link></p>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default Login
+export default Login;
