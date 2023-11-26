f754e9c46c825735d7c9d6a3906f6dd973f87dfb
Tushar Mahajan <tusharmahajan080@gmail.com>
2023-11-26 20:02:21 +0530

    Changes Done and Files Added

--- chitchathub-frontend/src/pages/Home.js
+++ chitchathub-frontend/src/pages/Home.js
@@ -1,8 +1,21 @@
 import React from "react";
-
+import {Row, Col,Button} from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap";
 function Home (){
     return(
-        <div>Home</div>
+        <Row>
+            <Col md={6} className="d-flex flex-direction-cloumn allign-items-center justify-content-center">
+                <div>
+                    <h1>Share your thoughts with the WORLD</h1>
+                    <p>Chit-Chat-Hub let you connect with the world!</p>
+                    <LinkContainer to="/chat">
+                        <Button variant="success">
+                            Get Started <i className="fas fa-comments home-message-icon"></i>
+                        </Button>
+                    </LinkContainer>
+                </div>
+            </Col>
+        </Row>
     )
 }
 
